d4feca5f4dcd4c500e5beb6768015fdc6dab4dc7
jakubkorczakowski <jakubkorczakowski@gmail.com>
2023-01-15 19:49:22 +0100

    wip

---
+++ flask_app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import repository
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+app = Flask(__name__)
+
+@app.route("/allocate", method=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"]
+    )
+
+    batchref = model.allocate(line, batches)
+
+    return {"batchref": batchref}, 201
+
+
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"],
+    )
+
+    if not is_valid_sku(line.sku, batches):
+        return {"message": f"Invalid sku {line.sku}"}, 400
+
+    try:
+        batchref = model.allocate(line, batches)
+    except model.OutOfStock as e:
+        return {"message": str(e)}, 400
+
+    session.commit()
+    return {"batchref": batchref}, 201
---
+++ test_services.py
@@ -0,0 +1,16 @@
+import repository
+
+class FakeRepository(repository.AbstractRepository):
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
+
+
